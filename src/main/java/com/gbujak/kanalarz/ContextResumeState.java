c7ad6e28ca235b10f4636fa2a2264857d165689f
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2026-03-04 20:19:11 +0100

    interim

--- src/main/java/com/gbujak/kanalarz/ContextResumeState.java
+++
@@ -1,7 +0,0 @@
-package com.gbujak.kanalarz;
-
-import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
-
-@NullMarked
-record ContextResumeState(@Nullable String basePath) {}
